cfc45726f3e90c61f0e7c4c9fa02e02fde4992ea
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-07-06 01:04:51 +0000

    first commit

---
+++ 0x02-funstions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-funstions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+ * _islower - return 1 if lower case else 0
+ *@c: character to check
+ *
+ * Return: 0 or 1 (Success)
+ */
+
+
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
