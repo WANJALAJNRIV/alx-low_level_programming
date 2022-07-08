e503fc02a3d3c08e3e117722dd4eef109d2975dc
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-07-08 01:58:35 +0000

    modifying file 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,30 +10,28 @@
 
 void print_triangle(int size)
 {
-	int i;
-	int z;
-	int d;
-	int p;
+	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		d = size -1;
-		for (i = 0; i < size; i++)
-		{
-			for (z = d;z > 0; z--)
-			{
-				_putchar(' ');
-			}
-			for (p = 0;p <= 1; p++)
-			{
-				_putchar (35);
-			}
-			d--;
-			_puchar('\n');
-		}
+		_putchar ('\n');
 	}
 	else
 	{
-		_putchar('\n');
+		for (i = 1; i  <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= (size - 1))
+				{
+					_putchar (' ');
+				}
+				else
+				{
+					_putchar ('#');
+				}
+			}
+			_putchar ('\n');
+		}
 	}
 }
