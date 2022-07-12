8135e68a40325f96f765f22c587c23b2db893758
WANJALAJNRIV <wanjalajnr04@gmail.com>
2022-07-12 01:00:49 +0000

    new directory on pointers arrays and strings

---
+++ 0x05-pionters_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string
+ * @str: Set of characters
+ *
+ * Return: Nothing
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pionters_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains file that have different functions on pointer, arrays and strings.
